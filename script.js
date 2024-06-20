9041a4d746de24cd65290fb3a1f64a21ae374035
joshuajoy <joshuajoy1108@gmail.com>
2024-06-20 20:10:20 +0530

    Second commit

--- script.js
+++ script.js
@@ -27,3 +27,52 @@
      }
  
  }, 1000);
+
+
+//  
+// w3schools Chart.js is used below
+
+const xValues = ["India", "China", "Russia", "USA", "Other"];
+const yValues = [50, 49, 44, 24, 20];
+const barColors = ["red", "green","blue","orange","black"];
+
+new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Participation from different nations %"
+      }
+    }
+  });
+
+
+  const x2Values = [2010,2012,2014,2016,2018,2020,2022,2024];
+  const y2Values = [7,8,9,9,10,14,15,16];
+  
+  new Chart("myChart2", {
+    type: "line",
+    data: {
+      labels: x2Values,
+      datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "rgba(0,0,255,0.1)",
+        data: y2Values
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        yAxes: [{ticks: {min: 6, max:16}}],
+      }
+    }
+  });
